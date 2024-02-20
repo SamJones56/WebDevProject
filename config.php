99f4042ff0eb55d7d8a2bd4a4bde28c7217d59b7
SamJones56 <b00156001@mytudublin.ie>
2024-02-20 13:30:46 +0000

    update password

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
  */
 $host = "localhost";
 $username = "root";
-$password = "";
+$password = "root";
 $dbname = "hotelTallafornia"; // will use later
 $dsn = "mysql:host=$host;dbname=$dbname"; // will use later
 $options = array(
